16b8c6361a7b9bb40f8ceb7da3f47a512df1645b
i850487 <thilaknath@ymail.com>
2024-06-09 12:56:34 -0400

    feat: modify code to include logging
    - add production readiness to code, connect to elastic cache in AWS
    - trigger only for code changes and not infra changes, add terraform exclude

--- main.go
+++ main.go
@@ -6,18 +6,22 @@ import (
     "fmt"
     "math/rand"
     "net/http"
+    "os"
     "time"
 
     "github.com/go-redis/redis/v8"
     "github.com/gorilla/mux"
+    "github.com/sirupsen/logrus"
 )
 
 var ctx = context.Background()
 var rdb *redis.Client
+var log = logrus.New()
 
 func init() {
+    redisAddr := fmt.Sprintf("%s:%s", os.Getenv("REDIS_ENDPOINT"), os.Getenv("REDIS_PORT"))
     rdb = redis.NewClient(&redis.Options{
-        Addr: "localhost:6379", // Use the appropriate address if running Redis elsewhere
+        Addr: redisAddr,
     })
 }
 
@@ -44,12 +48,18 @@ func shortenURL(w http.ResponseWriter, r *http.Request) {
     var request URLRequest
     err := json.NewDecoder(r.Body).Decode(&request)
     if err != nil {
+        log.WithError(err).Error("Failed to decode request body")
         http.Error(w, err.Error(), http.StatusBadRequest)
         return
     }
     shortURL := generateShortURL()
+    log.WithFields(logrus.Fields{
+        "shortURL": shortURL,
+        "longURL":  request.LongURL,
+    }).Info("Generated short URL")
     err = rdb.Set(ctx, shortURL, request.LongURL, 0).Err()
     if err != nil {
+        log.WithError(err).Error("Failed to save URL to Redis")
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
@@ -63,20 +73,29 @@ func redirectURL(w http.ResponseWriter, r *http.Request) {
     shortURL := vars["shortURL"]
     longURL, err := rdb.Get(ctx, shortURL).Result()
     if err == redis.Nil {
+        log.WithField("shortURL", shortURL).Warn("Short URL not found")
         http.Error(w, "URL not found", http.StatusNotFound)
         return
     } else if err != nil {
+        log.WithError(err).Error("Failed to get URL from Redis")
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
+    log.WithFields(logrus.Fields{
+        "shortURL": shortURL,
+        "longURL":  longURL,
+    }).Info("Redirecting to long URL")
     http.Redirect(w, r, longURL, http.StatusFound)
 }
 
 func main() {
+    log.SetFormatter(&logrus.JSONFormatter{})
+    log.SetLevel(logrus.InfoLevel)
+
     r := mux.NewRouter()
     r.HandleFunc("/shorten", shortenURL).Methods("POST")
     r.HandleFunc("/{shortURL}", redirectURL).Methods("GET")
     http.Handle("/", r)
-    fmt.Println("Server is running on port 8080")
+    log.Info("Server is running on port 8080")
     http.ListenAndServe(":8080", nil)
 }
